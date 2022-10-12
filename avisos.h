520c64f0626f66884da793980cc3de35294c6d26
FelipeSouza14 <felipesouzabenicio@gmail.com>
2022-10-12 17:35:30 -0300

    assinatura de funções do módulo avisos

---
+++ avisos.h
@@ -0,0 +1,7 @@
+void navegacaoCrudAvisos(void);
+char crudAvisos(void);
+void cadastroAvisos(void);
+void buscarAvisos(void);
+void atualizarAvisos(void);
+char oqueAtualizarAvisos(void);
+void deletarAvisos(void);
