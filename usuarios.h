fed18965034fe81d9ba214f5067c96215253b1c3
Isa Kaillany <isakaillany123@gmail.com>
2022-10-26 17:55:04 -0300

    add struct no .h

--- usuarios.h
+++ usuarios.h
@@ -9,3 +9,15 @@ void usuariosCadastrados(void);
 void navegacaoUsuariosCadastrados(void);
 char departamentoUsuario(void);
 int escolhaCargo(void);
+
+
+typedef struct usuarios Usuarios;
+
+struct usuarios 
+{
+  char nome[50];
+  char email[50];
+  char senha[9];
+  char telefone[12];
+  char id[7];
+};
