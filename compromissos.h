0d1a5195d66c82649aac2deb936dc4cf40179848
FelipeSouza14 <felipesouzabenicio@gmail.com>
2022-10-27 21:00:31 -0300

    Criando o Struct de compromissos

--- compromissos.h
+++ compromissos.h
@@ -6,3 +6,16 @@ void atualizarCompromissos(void);
 char oqueAtualizarCompromissos(void);
 void deletarCompromissos(void);
 int escolhaDepartamento(void);
+
+
+
+typedef struct compromissos Compromissos;
+
+struct compromissos {
+  char titulo[100];
+  char descricao[1000];
+  char data[6];
+  char id[7];
+  int hora;
+  int min;
+};
