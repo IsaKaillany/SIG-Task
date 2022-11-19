2d1632abb482710aefa3f72c1acb1e307cc1370d
Isa Kaillany <isakaillany123@gmail.com>
2022-11-19 20:15:59 -0300

    Implementação do gerador de ID

--- usuarios.c
+++ usuarios.c
@@ -60,8 +60,10 @@ char telaUsuarios(void)
 
 void cadastroUsuario(void)
 {
-    char idAux[7];
+    int idAux;
+    char idString[8];
     Usuarios* usu;
+
     usu = (Usuarios*) malloc(sizeof(Usuarios));
     system("clear||cls");
     printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
@@ -93,17 +95,24 @@ void cadastroUsuario(void)
     {
         usu->departamento = escolhaDepartamento();
     }    
-    //Por enquanto vai ser manual (Colocar o depart no final do id, posteriormente)
-    do
-    {
-        printf("ID [6 digitos]: "); //INICIANDO COM 11 É IGUAL À GERENCIA E 22 É IGUAL À FUNCIONÁRIO
-        scanf(" %[0-9]", idAux);
-        getchar();
-    } while (validaID(idAux));
-    if (validaID(idAux) == 0)
-    {
-        strcpy(usu->id, idAux);
-    }
+
+    idAux = geraID(usu->cargo);
+    itoa(idAux, idString, 10); //Transforma int em char
+    strcpy(usu->id, idString);
+    printf("Anote seu ID: %s", idString);
+
+    //Será implementado depois com a validação
+    // do
+    // {   
+    //     idAux = geraID(usu->cargo);
+    //     itoa(idAux, idString, 10);
+    // } while (validaID(idString));
+    
+    // if (validaID(idString) == 0)
+    // {
+    //     strcpy(usu->id, idString);
+    //     printf("Seu ID eh: %s", idString);
+    // }
 
     do
     {
